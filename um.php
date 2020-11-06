b9b1ad0ebc4d1df60cc7a3b268111013d3aef7e4
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-11-06 09:55:06 +0000

    Update um.php

--- um.php
+++ um.php
@@ -1,6 +1,9 @@
 <?php
+define('PI', 3.14)
+$raio = rand(1,500);
 
-$raio = 6;
-
-a = 3.14 * $raio / 2;
-echo ' A area é ' .$raio;
+function area ($raio){
+	$area = $raio * $raio * PI;
+	return $a;
+}
+echo 'a area do circulo com raio' .$raio. 'e'.area($raio);
